5c42a83594f9679e84f664c8f512638a7c577357
MiriamCall <bow18005@byui.edu>
2025-02-08 00:43:16 -0700

    update user model to include timestamps and set default for favorites; modify API routes for consistency

--- models/user.js
+++ models/user.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  favorites: [{ type: Number }],
-});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    favorites: { type: [Number], default: [] },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
+
 module.exports = User;
